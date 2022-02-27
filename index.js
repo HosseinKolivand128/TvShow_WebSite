e5c0d8470e72a72d7dbdcbe4c605e4b48e1c7a8b
HosseinKolivand128 <ac.hossein.ac@gmail.com>
2022-02-27 18:59:39 +0330

    updated

--- index.js
+++ index.js
@@ -92,9 +92,23 @@ function showEpisodes(data) {
   const descabout = document.querySelector("#desc-about");
   descabout.innerHTML = data.summary;
   const episodes = getEpisodes(data.id);
+  
+  const thead = document.querySelector("thead");
+  removeAllChildNodes(thead);
+
   const tBody = document.querySelector("#table-Body");
   removeAllChildNodes(tBody);
   try {
+    const trH = document.createElement("tr");
+    trH.classList.add("table-secondary");
+    const text = ["Image", "Episode Info", "Summary", ""];
+    for (let i = 0; i < 4; i++) {
+      const th = document.createElement("th");
+      th.textContent = text[i];
+      th.classList.add("table-primary", "fs-5");
+      trH.appendChild(th);
+    }
+    thead.appendChild(trH);
     episodes.then((element) => {
       console.log(element);
       // console.log(element.length);
@@ -133,6 +147,7 @@ function showEpisodes(data) {
 
           const btn = document.createElement("button");
           btn.textContent = "more details";
+          btn.classList.add("btn-warning");
           btnTd.appendChild(btn);
           tr.appendChild(btnTd);
 
@@ -158,50 +173,3 @@ async function getEpisodes(id) {
   return data;
 }
 
-// setImage();
-
-//   const response = [];
-//   for (let i = 0; i < 3; i++) {
-//     response[i] = await fetch(`https://api.tvmaze.com/shows/${i + 3}/images`);
-//     const data = response[i].json();
-//     const section = document.createElement("section");
-//     const container = document.querySelector(".carousel-item");
-//     try {
-//       data.then((element) => {
-//         section.style.background = `url(${element[0].resolutions.medium.url})`;
-//         section.style.backgroundColor = "red";
-//         section.style.height = "100px";
-//         container.append(section);
-//       });
-//     } catch (error) {}
-//   }
-// }
-
-// async function setAdImage(url) {
-//   const response = [];
-//   for (let i = 0; i < adImage.length; i++) {
-//     response[i] = await fetch(`https://api.tvmaze.com/shows/${i + 3}/images`);
-//     const data = response[i].json();
-//     try {
-//       data.then((element) => {
-//         adImage[i].src = element[0].resolutions.medium.url;
-//       });
-//     } catch (error) {
-//       console.log(error);
-//     }
-// try {
-//   data.then((element) => {
-//     console.log(container[i]);
-
-//     container[
-//       i
-//     ].style.background = `url(${element[0].resolutions.medium.url})`;
-//   });
-// } catch (error) {
-//   console.log(error);
-// }
-//   }
-//   console.log(response);
-// }
-
-// adImage.src = element[0].resolutions.medium.url;
